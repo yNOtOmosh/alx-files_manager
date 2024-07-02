9f1e5666c385fdfd35979e1f7bd076d14f1cd274
yNOtOmosh <onyango.omondi.tony@gmail.com>
2024-07-02 14:44:07 +0300

    Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -6,4 +6,42 @@ import { createClient } from 'redis';
  */
 
 class RedisClient {
-  
+    constructor() {
+        this.client = createClient();
+        this.client.on('error', (err) => console.log('Redis Client Error:', err));
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, reply) => {
+                if (err) return reject(err);
+                resolve(reply);
+            });
+        });
+    }
+
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.set(key, value, 'EX', duration, (err, reply) => {
+                if (err) return reject(err);
+                resolve(reply);
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) return reject(err);
+                resolve(reply);
+            });
+        });
+    }
+}
+
+export const redisClient = new RedisClient();
+export default redisClient;
