3be9442b993b685c789bc0a90ac17571e48daade
yNOtOmosh <onyango.omondi.tony@gmail.com>
2024-07-02 11:35:36 -0700

    Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -6,41 +6,34 @@ import { createClient } from 'redis';
  */
 
 class RedisClient {
-    constructor() {
-        this.client = createClient();
-        this.client.on('error', (err) => console.log('Redis Client Error:', err));
-    }
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.isClientConnected;
+  }
 
-    async get(key) {
-        return new Promise((resolve, reject) => {
-            this.client.get(key, (err, reply) => {
-                if (err) return reject(err);
-                resolve(reply);
-            });
-        });
-    }
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
 
-    async set(key, value, duration) {
-        return new Promise((resolve, reject) => {
-            this.client.set(key, value, 'EX', duration, (err, reply) => {
-                if (err) return reject(err);
-                resolve(reply);
-            });
-        });
-    }
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX)
+      .bind(this.client)(key, duration, value);
+  }
 
-    async del(key) {
-        return new Promise((resolve, reject) => {
-            this.client.del(key, (err, reply) => {
-                if (err) return reject(err);
-                resolve(reply);
-            });
-        });
-    }
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
 }
 
 export const redisClient = new RedisClient();
